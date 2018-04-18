bd38e34c1b725e356a25bd35a232bb7b16d24229
Eric Wong <ewong11@stuy.edu>
2018-04-18 10:06:06 -0400

    added Deque

---
+++ lab02/Deque.java
@@ -0,0 +1,14 @@
+pubic interface Deque extends Queue {
+    //public boolean add(String s);
+
+    public void addFirst(String s);
+
+    public void addLast(String s);
+
+    public String getFirst();
+
+    public String getLast();
+
+    //public String peek();
+
+    public int size();
