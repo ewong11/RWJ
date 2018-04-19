b8d781fc2ff73aae1e420006f789847b4af9969a
jkim33 <jkim33@stuy.edu>
2018-04-19 00:11:30 -0400

    adding 2 methods

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,34 +1,39 @@
+//Team RWJ - Jason Kim, Raymond Wu, Eric Wong
+//APCS2 pd8
+//LAB#02 -- All Hands on Deque
+//2018-04-18
+
 public class QQKachoo implements Deque {
     //instance variables
     DLLNode head, end;
     int size;
 
     public QQKachoo() {
-      head = end = null;
-      size = 0;
+	head = end = null;
+	size = 0;
     }
     public void addFirst(String s) {
         head = new DLLNode(s, null, head);
         if (size == 0) {
-          end = head;
-          size++;
-      }
-      else {
-        head.getNext().setPrev(head);
-        size++;
-    }
+	    end = head;
+	    size++;
+	}
+	else {
+	    head.getNext().setPrev(head);
+	    size++;
+	}
     }
 
     public void addLast(String s) {
-      end = new DLLNode(s, end, null);
-      if (size == 0) {
-        head = end;
-        size++;
-      }
-      else {
-        end.getPrev().setNext(end);
-        size++;
-      }
+	end = new DLLNode(s, end, null);
+	if (size == 0) {
+	    head = end;
+	    size++;
+	}
+	else {
+	    end.getPrev().setNext(end);
+	    size++;
+	}
     }
 
     public String removeFirst() {
@@ -45,22 +50,66 @@ public class QQKachoo implements Deque {
 	return retStr;	
     }
 
-    
-/*
-    public String toString() {
-      String retStr = "";
-      for (int i =0; i < size; i++ ) {
-        retStr +=
-      }
+    public boolean removeFirstOccurence (String s) {
+	DLLNode temp = head; //create an alias
+	if (head.getCargo().equals(s)) {
+	    head = head.getNext();
+	    head.setPrev(null);
+	    return true;
+	}
+	while (temp.getNext() != null) {
+	    if (temp.getNext().getCargo().equals(s)) {
+		temp.setNext(temp.getNext().getNext());
+		temp.getNext().setPrev(temp);
+		return true;
+	    }
+	    temp = temp.getNext();
+	}
+	if (end.getCargo().equals(s)) {
+	    end = end.getPrev();
+	    end.setNext(null);
+	    return true;
+	}
+	return false;
     }
-    */
 
+    public boolean removeLastOccurence (String s) {
+	DLLNode temp = end; //create an alias
+	if (end.getCargo().equals(s)) {
+	    end = end.getPrev();
+	    end.setNext(null);
+	    return true;
+	}
+	while (temp.getPrev() != null) {
+	    if (temp.getPrev().getCargo().equals(s)) {
+		temp.setPrev(temp.getPrev().getPrev());
+		temp.getPrev().setNext(temp);
+		return true;
+	    }
+	    temp = temp.getPrev();
+	}
+	if (head.getCargo().equals(s)) {
+	    head = head.getNext();
+	    head.setPrev(null);
+	    return true;
+	}
+	return false;
+    }
+	
+    public String toString() {
+	String retStr = "";
+	DLLNode temp = head;
+        while (temp != null) {
+	    retStr += temp.getCargo() + " ";
+	    temp = temp.getNext();
+	}
+    }
     public static void main(String[] args) {
-      QQKachoo test = new QQKachoo();
+	QQKachoo test = new QQKachoo();
 
-      test.addFirst("this");
-      test.addFirst("is");
-      test.addFirst("us");
-      test.addFirst("fin");
+	test.addFirst("this");
+	test.addFirst("is");
+	test.addFirst("us");
+	test.addFirst("fin");
     }
 }
